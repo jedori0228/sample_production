9c6b38a2d1deb2afcd288b87f0be1c795246c08b
Jae Sung Kim <jskim@cern.ch>
2016-12-14 03:11:29 +0900

    type

--- SIM_to_RAWSIM/CRAB_KNU_TIER2/submit_crab.sh
+++ SIM_to_RAWSIM/CRAB_KNU_TIER2/submit_crab.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                                                                                                                                                     
 
-channel=MuMuMU
-
-#for i in 5 10 20 30 40 50 60 70 90 100 150 200 300 400 500 700 1000
-for i in 10 30 50 70
+channel=MupEp
+for i in 40
 do
-  cd "crab_"$channel"_"$i
+  cd "crab_MajoranaNeutrinoTo"$channel"_"$i
   echo "############################"
-  echo "sumbmiting "crab_"$channel"_"$i.py
-  crab submit -c crab_"$channel"_$i.py
+  echo "sumbmiting crab_"$channel"_"$i.py
+  crab submit -c crab_MajoranaNeutrinoTo"$channel"_$i.py
   cd ..
 done
