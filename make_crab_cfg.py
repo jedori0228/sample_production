3e8769e8af1048af81e87813af9777b4910ea692
Jae Sung Kim <jskim@cern.ch>
2018-03-19 16:37:17 +0900

    script added

---
+++ make_crab_cfg.py
@@ -0,0 +1,42 @@
+import os
+
+## write sample alias and lhe path, starting with /store/
+## e.g.,
+## PairProduction_MuMu_ZR3000_N200_WR5000_NLO /store/user/jskim/SE_UserHome/lhe/LRSM/PairProduction_MuMu_ZR3000_N200_WR5000_NLO/events.lhe
+
+txtfilename = 'filelist.txt'
+samples = open(txtfilename).readlines()
+
+for AAA in samples:
+
+  words = AAA.split()
+
+  sample = words[0]
+  lhefilepath = words[1]
+
+  dirname = 'crab_'+sample
+  os.system('mkdir '+dirname)
+
+  cfg_skleton = open('skeletons/LHE__TO__GEN-SIM.py').readlines()
+  output_cfg = open(dirname+'/LHE__TO__GEN-SIM__'+sample+'.py','w')
+  for line in cfg_skleton:
+    if "##INPUT_HERE##" in line:
+      for lheindex in range(0, 1):
+        output_cfg.write("      'root://cluster142.knu.ac.kr/"+lhefilepath+"',\n")
+    else:
+      output_cfg.write(line)
+  output_cfg.close()
+
+  crabpy_skeleton = open('skeletons/crab_skeleton.py').readlines()
+  output_crabpy = open(dirname+'/crab_'+sample+'.py', 'w')
+  for line in crabpy_skeleton:
+    if "##requestName##" in line:
+      output_crabpy.write("config.General.requestName = '"+sample+"_CMSSW_9_3_4_GEN-SIM'\n")
+    elif "##psetName##" in line:
+      output_crabpy.write("config.JobType.psetName = 'LHE__TO__GEN-SIM__"+sample+".py'\n")
+    elif "##outputPrimaryDataset##" in line:
+      output_crabpy.write("config.Data.outputPrimaryDataset = '"+sample+"'\n")
+    else:
+      output_crabpy.write(line)
+  output_crabpy.close()
+
