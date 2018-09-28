79c761d5af3f5450b0fcf30e97bc5145cdbb8a95
shjeon <shjeon@cern.ch>
2018-09-28 10:05:01 +0900

    modify numcores and site white/blacklist

--- AODSIM__TO__MINIAODSIM/skeletons/AODSIM__TO__MINIAODSIM.py
+++ AODSIM__TO__MINIAODSIM/skeletons/AODSIM__TO__MINIAODSIM.py
@@ -153,8 +153,8 @@ from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
 associatePatAlgosToolsTask(process)
 
 #Setup FWK for multithreaded
-#process.options.numberOfThreads=cms.untracked.uint32(4)
-#process.options.numberOfStreams=cms.untracked.uint32(0)
+process.options.numberOfThreads=cms.untracked.uint32(4)
+process.options.numberOfStreams=cms.untracked.uint32(0)
 
 #do not add changes to your config after this point (unless you know what you are doing)
 from FWCore.ParameterSet.Utilities import convertToUnscheduled
